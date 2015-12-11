7198c6b53cd0e6c1b44ee6e4976e7ef62e943570
manish-shukla <manish.shukla@cisinlabs.com>
2015-12-11 14:09:28 +0530

    Customizing the Capistrano recipe.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'testcap'
+set :application, ''
 set :repo_url, 'git@github.com:kuldeepcis/master1.git'
 
 # Default branch is :master
