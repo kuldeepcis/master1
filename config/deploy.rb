e69124e6128d3702d14f3f07211f1d09304e7117
manish-shukla <manish.shukla@cisinlabs.com>
2015-12-11 13:57:27 +0530

    Customizing the Capistrano recipe.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,25 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'testcap'
+set :repo_url, 'git@github.com:kuldeepcis/master1.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/dlrmedia/sites/werksters/public_html/testcap'
 
 # Default value for :scm is :git
-# set :scm, :git
-
+set :scm, :git
+set :user, "dlrmedia"
 # Default value for :format is :pretty
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
-
+set :log_level, :debug
+set :stages, ["staging", "production"]
+set :default_stage, "staging"
 # Default value for :pty is false
 # set :pty, true
 
