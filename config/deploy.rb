eb375d00894bc5d3ff8eb4e2031e94c0b053a4a6
manish-shukla <manish.shukla@cisinlabs.com>
2015-12-11 16:10:16 +0530

    Customizing the Capistrano recipe.

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,13 +3,15 @@ lock '3.4.0'
 
 set :application, ''
 set :repo_url, 'git@github.com:kuldeepcis/master1.git'
-set :user, "deploy"
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/home/dlrmedia/sites/werksters/public_html/testcap'
 
+set :branch, "master"
+set :user, "deploy"
 # Default value for :scm is :git
 set :scm, :git
 
