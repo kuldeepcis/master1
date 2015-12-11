488569760c364b1b485803dfe612ddcb2f7121f0
manish-shukla <manish.shukla@cisinlabs.com>
2015-12-11 15:23:06 +0530

    Customizing the Capistrano recipe.

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :deploy_to, '/home/dlrmedia/sites/werksters/public_html/testcap'
 
 # Default value for :scm is :git
 set :scm, :git
-set :user, "dlrmedia"
+set :user, "deploy"
 # Default value for :format is :pretty
 # set :format, :pretty
 set :use_sudo, true
